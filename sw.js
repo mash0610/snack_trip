59c54b90809170db8eeccb1a1b85b00b39b34bff
mash0610 <hideoki1978@gmail.com>
2026-06-07 13:37:33 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE = 'snack-trip-v1';
+const ASSETS = ['./snack_trip_v4.html', './manifest.json', './icon.svg'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
